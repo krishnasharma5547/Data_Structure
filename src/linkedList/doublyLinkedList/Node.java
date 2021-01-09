309be2e0b9739e620e07590778e10026772dc6bf
krishnasharma5547 <krishna.sharma_cs18@gla.ac.in>
2021-01-09 12:13:18 +0530

    node class for doubly linked list created

---
+++ src/linkedList/doublyLinkedList/Node.java
@@ -0,0 +1,31 @@
+package linkedList.doublyLinkedList;
+
+public class Node {
+    private int data;
+    private Node prev;
+    private Node next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
